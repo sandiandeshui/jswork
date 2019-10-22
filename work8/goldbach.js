4a487748d731ec6406a85bf85b40e45c6d761232
sandiandeshui <1247455014@qq.com>
2019-10-22 16:06:18 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,54 +1,27 @@
-// var num = parseInt(prompt("输入一个偶数"));
-//     isTrue(num);
-//     // 判断一个数是否是质数
-//     function factorial(a){
-//         var sum=0;
-//         for(var i=1;i<=a;i++){
-//             if(a % i ==0){
-//                 sum++;
-//             }
-//         }
-//         if(sum==2){
-//             return true;
-//         }else{
-//             return false;
-//         }
-//     }
-//   // 判断用户输入的偶数是否可分为两个质数之和：
-
-//     function isTrue(a){
-//       for(var i=2;i<a-2;i++){
-//         var j = a -i;
-//         if(factorial(i)&&factorial(j)){
-//             console.log(a+"可以拆分为两个质数"+ i+"与"+j+"的和");
-//         }
-//       }
-//     }
-for (var i=8;i<10000;i+=2){//遍历8-10000的所有偶数，去判断每一个偶数是否满足哥德巴赫猜想
-    if(isgdbh(i)){
-        document.write(i +"满足哥德巴赫猜想"+"</br>");
-    }else{
-        document.write(i + "不满足哥德巴赫猜想"+"</br>");
-    }
-}
-function isgdbh(num){
-   var flag = false;
-   for(var i=2;i<num;i++){
-       if(isprime(i)&& isprime(num - i)){
-           flag = true;
-           break;
-       }else{
-           continue;
-       }
-   }
-   return flag;
-}
-function isprime(num){
-   var flag = true;
-   for(var j=2;j<num;j++){
-       if(num % j ==0){
-           flag = false;
-       }
-   }
-   return flag;
-}
+function goldbach() {
+  var value = document.getElementById('odd').value 
+   if(value%2!==0){         alert("请输入偶数") 
+}    
+   let goldbach = document.getElementById('goldbach')     
+   var arr =[]     
+    var a = 0;     
+    for(var i=2;i<=value;i++){ 
+           a = 0      
+    for(var j=2;j<i;j++){             
+         if(i%j==0){           
+            a++           
+    }     
+   }         
+     if(a==0){    
+     arr.push(i) 
+   }   
+     }    
+      var str = ''   
+      for(let i=0;i<(arr.length)/2;i++){ 
+      for(let j=0;j<arr.length;j++){    
+    if((arr[i]+arr[j])===Number(value)){   
+      str += '<div>'+value+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+ '</div>'            
+    }        
+}     
+}     
+goldbach.innerHTML = str }
